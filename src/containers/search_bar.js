9a66f86a1ea69c230f822924d2fffd0d4463865f
Max <maksimshuster@Maksims-MacBook-Pro.local>
2016-07-18 14:43:08 -0700

    Ajax request returns 200

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,42 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
 
-export default class SearchBar extends Component {
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event){
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
+         
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
+    }
+
     render() {
         return(
-            <form className="input-group">
-                <input />
+            <form 
+            className="input-group"
+            onSubmit={this.onFormSubmit}
+            >
+                <input
+                    placeholder="Get Forecast"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
@@ -13,3 +44,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
